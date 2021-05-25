e65caa988513cc701df0a21f6101382f2a0dd36d
WJB3 <2459666474@qq.com>
2021-05-25 18:10:41 +0800

    fix

--- src/classNames.ts
+++ src/classNames.ts
@@ -1,4 +1,13 @@
-
-export default function(){
+ 
+import {
     
-}
+} from '@parrotjs/rc-utils-whichtype';
+
+export default function classNames(...args){
+    let classes=[];
+    for(let i=0;i<arguments.length;i++){
+        let arg = arguments[i];
+
+        if(arg)
+    }
+}   
